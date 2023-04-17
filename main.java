126b9e600668a6a17e3c5f335d2af94333581b77
NikhilDongre1 <91336150+NikhilDongre1@users.noreply.github.com>
2023-04-17 08:03:16 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,245 +1,298 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.*;
-import javax.swing.*;
-import javax.swing.event.*;
-import java.util.Random;
 
-public class TicTacToe extends JFrame implements ChangeListener, ActionListener {
-  private JSlider slider;
-  private JButton oButton, xButton;
-  private Board board;
-  private int lineThickness=4;
-  private Color oColor=Color.BLUE, xColor=Color.RED;
-  static final char BLANK=' ', O='O', X='X';
-  private char position[]={  // Board position (BLANK, O, or X)
-    BLANK, BLANK, BLANK,
-    BLANK, BLANK, BLANK,
-    BLANK, BLANK, BLANK};
-  private int wins=0, losses=0, draws=0;  // game count by user
+class Myclass{
+Myclass(){
+    // image imported
+ImageIcon cross=new ImageIcon("cross.jpg");
+ImageIcon circle=new ImageIcon("circle.jpg");
 
-  // Start the game
-  public static void main(String args[]) {
-    new TicTacToe();
-  }
+//Frame
+//container that provides a window on the screen
+JFrame jf=new JFrame();
 
-  // Initialize
-  public TicTacToe() {
-    super("Tic Tac Toe demo");
-    JPanel topPanel=new JPanel();
-    topPanel.setLayout(new FlowLayout());
-    topPanel.add(new JLabel("Line Thickness:"));
-    topPanel.add(slider=new JSlider(SwingConstants.HORIZONTAL, 1, 20, 4));
-    slider.setMajorTickSpacing(1);
-    slider.setPaintTicks(true);
-    slider.addChangeListener(this);
-    topPanel.add(oButton=new JButton("O Color"));
-    topPanel.add(xButton=new JButton("X Color"));
-    oButton.addActionListener(this);
-    xButton.addActionListener(this);
-    add(topPanel, BorderLayout.NORTH);
-    add(board=new Board(), BorderLayout.CENTER);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setSize(500, 500);
-    setVisible(true);
-  }
+// position our components absolutely
+jf.setLayout(null);
+//window size set
+jf.setSize(310,450);
+jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+jf.setLocationRelativeTo(null);
+jf.setVisible(true); 
+JButton jb1=new JButton();
+JButton jb2=new JButton();
+JButton jb3=new JButton();
+JButton jb4=new JButton();
+JButton jb5=new JButton();
+JButton jb6=new JButton();
+JButton jb7=new JButton();
+JButton jb8=new JButton();
+JButton jb9=new JButton();
+JButton jb10=new JButton("RESET");
+JLabel jl=new JLabel();
+Font f=new Font("SansSerif",1,30);
+jl.setFont(f);
+jl.setText("Player :1");
+//add
+jf.add(jb1);
+jf.add(jb2);
+jf.add(jb3);
+jf.add(jb4);
+jf.add(jb5);
+jf.add(jb6);
+jf.add(jb7);
+jf.add(jb8);
+jf.add(jb9);
+jf.add(jl);
+jf.add(jb10);
 
-  // Change line thickness
-  public void stateChanged(ChangeEvent e) {
-    lineThickness = slider.getValue();
-    board.repaint();
-  }
+//
+jb1.setBounds(0,50,100,100);
+jb2.setBounds(100,50,100,100);
+jb3.setBounds(200,50,100,100);
+jb4.setBounds(0,150,100,100);
+jb5.setBounds(100,150,100,100);
+jb6.setBounds(200,150,100,100);
+jb7.setBounds(0,250,100,100);
+jb8.setBounds(100,250,100,100);
+jb9.setBounds(200,250,100,100);
+jl.setBounds(30,0,270,50);
+jb10.setBounds(0,370,300,30);
 
-  // Change color of O or X
-  public void actionPerformed(ActionEvent e) {
-    if (e.getSource()==oButton) {
-      Color newColor = JColorChooser.showDialog(this, "Choose a new color for O", oColor);
-      if (newColor!=null)
-        oColor=newColor;
-    }
-    else if (e.getSource()==xButton) {
-      Color newColor = JColorChooser.showDialog(this, "Choose a new color for X", xColor);
-      if (newColor!=null)
-        xColor=newColor;
-    }
-    board.repaint();
-  }
+class MyListener implements ActionListener{
+int flag=0;
+int c1=0,c2=0,c3=0,c4=0,c5=0,c6=0,c7=0,c8=0,c9=0;
+int x1=0,x2=0,x3=0,x4=0,x5=0,x6=0,x7=0,x8=0,x9=0;
+public void actionPerformed(ActionEvent ae){   //abstract method
 
-  // Board is what actually plays and displays the game
-  private class Board extends JPanel implements MouseListener {
-    private Random random=new Random();
-    private int rows[][]={{0,2},{3,5},{6,8},{0,6},{1,7},{2,8},{0,8},{2,6}};
-      // Endpoints of the 8 rows in position[] (across, down, diagonally)
 
-    public Board() {
-      addMouseListener(this);
-    }
+if(ae.getSource()==jb1){
+if(flag==0){
+jb1.setIcon(cross);
+flag=1;x1=1;
+jl.setText("Player :2");
+}
+else{
+jb1.setIcon(circle);
+flag=0;c1=1;
+jl.setText("Player :1");
+}
 
-    // Redraw the board
-    public void paintComponent(Graphics g) {
-      super.paintComponent(g);
-      int w=getWidth();
-      int h=getHeight();
-      Graphics2D g2d = (Graphics2D) g;
+}
 
-      // Draw the grid
-      g2d.setPaint(Color.WHITE);
-      g2d.fill(new Rectangle2D.Double(0, 0, w, h));
-      g2d.setPaint(Color.BLACK);
-      g2d.setStroke(new BasicStroke(lineThickness));
-      g2d.draw(new Line2D.Double(0, h/3, w, h/3));
-      g2d.draw(new Line2D.Double(0, h*2/3, w, h*2/3));
-      g2d.draw(new Line2D.Double(w/3, 0, w/3, h));
-      g2d.draw(new Line2D.Double(w*2/3, 0, w*2/3, h));
+if(ae.getSource()==jb2){
+if(flag==0){
+jb2.setIcon(cross);
+flag=1;x2=1;
+jl.setText("Player :2");
+}
+else{
+jb2.setIcon(circle);
+flag=0;c2=1;
+jl.setText("Player :1");
+}
 
-      // Draw the Os and Xs
-      for (int i=0; i<9; ++i) {
-        double xpos=(i%3+0.5)*w/3.0;
-        double ypos=(i/3+0.5)*h/3.0;
-        double xr=w/8.0;
-        double yr=h/8.0;
-        if (position[i]==O) {
-          g2d.setPaint(oColor);
-          g2d.draw(new Ellipse2D.Double(xpos-xr, ypos-yr, xr*2, yr*2));
-        }
-        else if (position[i]==X) {
-          g2d.setPaint(xColor);
-          g2d.draw(new Line2D.Double(xpos-xr, ypos-yr, xpos+xr, ypos+yr));
-          g2d.draw(new Line2D.Double(xpos-xr, ypos+yr, xpos+xr, ypos-yr));
-        }
-      }
-    }
+}
 
-    // Draw an O where the mouse is clicked
-    public void mouseClicked(MouseEvent e) {
-      int xpos=e.getX()*3/getWidth();
-      int ypos=e.getY()*3/getHeight();
-      int pos=xpos+3*ypos;
-      if (pos>=0 && pos<9 && position[pos]==BLANK) {
-        position[pos]=O;
-        repaint();
-        putX();  // computer plays
-        repaint();
-      }
-    }
+if(ae.getSource()==jb3){
+if(flag==0){
+jb3.setIcon(cross);
+flag=1;x3=1;
+jl.setText("Player :2");
+}
+else{
+jb3.setIcon(circle);
+flag=0;c3=1;
+jl.setText("Player :1");
+}
 
-    // Ignore other mouse events
-    public void mousePressed(MouseEvent e) {}
-    public void mouseReleased(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
+}
 
-    // Computer plays X
-    void putX() {
-      
-      // Check if game is over
-      if (won(O))
-        newGame(O);
-      else if (isDraw())
-        newGame(BLANK);
+if(ae.getSource()==jb4){
+if(flag==0){
+jb4.setIcon(cross);
+flag=1;x4=1;
+jl.setText("Player :2");
+}
+else{
+jb4.setIcon(circle);
+flag=0;c4=1;
+jl.setText("Player :1");
+}
 
-      // Play X, possibly ending the game
-      else {
-        nextMove();
-        if (won(X))
-          newGame(X);
-        else if (isDraw())
-          newGame(BLANK);
-      }
-    }
+}
 
-    // Return true if player has won
-    boolean won(char player) {
-      for (int i=0; i<8; ++i)
-        if (testRow(player, rows[i][0], rows[i][1]))
-          return true;
-      return false;
-    }
+if(ae.getSource()==jb5){
+if(flag==0){
+jb5.setIcon(cross);
+flag=1;x5=1;
+jl.setText("Player :2");
+}
+else{
+jb5.setIcon(circle);
+flag=0;c5=1;
+jl.setText("Player :1");
+}
 
-    // Has player won in the row from position[a] to position[b]?
-    boolean testRow(char player, int a, int b) {
-      return position[a]==player && position[b]==player 
-          && position[(a+b)/2]==player;
-    }
+}
 
-    // Play X in the best spot
-    void nextMove() {
-      int r=findRow(X);  // complete a row of X and win if possible
-      if (r<0)
-        r=findRow(O);  // or try to block O from winning
-      if (r<0) {  // otherwise move randomly
-        do
-          r=random.nextInt(9);
-        while (position[r]!=BLANK);
-      }
-      position[r]=X;
-    }
+if(ae.getSource()==jb6){
+if(flag==0){
+jb6.setIcon(cross);
+flag=1;x6=1;
+jl.setText("Player :2");
+}
+else{
+jb6.setIcon(circle);
+flag=0;c6=1;
+jl.setText("Player :1");
+}
 
-    // Return 0-8 for the position of a blank spot in a row if the
-    // other 2 spots are occupied by player, or -1 if no spot exists
-    int findRow(char player) {
-      for (int i=0; i<8; ++i) {
-        int result=find1Row(player, rows[i][0], rows[i][1]);
-        if (result>=0)
-          return result;
-      }
-      return -1;
-    }
+}
 
-    // If 2 of 3 spots in the row from position[a] to position[b]
-    // are occupied by player and the third is blank, then return the
-    // index of the blank spot, else return -1.
-    int find1Row(char player, int a, int b) {
-      int c=(a+b)/2;  // middle spot
-      if (position[a]==player && position[b]==player && position[c]==BLANK)
-        return c;
-      if (position[a]==player && position[c]==player && position[b]==BLANK)
-        return b;
-      if (position[b]==player && position[c]==player && position[a]==BLANK)
-        return a;
-      return -1;
-    }
+if(ae.getSource()==jb7){
+if(flag==0){
+jb7.setIcon(cross);
+flag=1;x7=1;
+jl.setText("Player :2");
+}
+else{
+jb7.setIcon(circle);
+flag=0;c7=1;
+jl.setText("Player :1");
+}
 
-    // Are all 9 spots filled?
-    boolean isDraw() {
-      for (int i=0; i<9; ++i)
-        if (position[i]==BLANK)
-          return false;
-      return true;
-    }
+}
 
-    // Start a new game
-    void newGame(char winner) {
-      repaint();
+if(ae.getSource()==jb8){
+if(flag==0){
+jb8.setIcon(cross);
+flag=1;x8=1;
+jl.setText("Player :2");
+}
+else{
+jb8.setIcon(circle);
+flag=0;c8=1;
+jl.setText("Player :1");
+}
 
-      // Announce result of last game.  Ask user to play again.
-      String result;
-      if (winner==O) {
-        ++wins;
-        result = "You Win!";
-      }
-      else if (winner==X) {
-        ++losses;
-        result = "I Win!";
-      }
-      else {
-        result = "Tie";
-        ++draws;
-      }
-      if (JOptionPane.showConfirmDialog(null, 
-          "You have "+wins+ " wins, "+losses+" losses, "+draws+" draws\n"
-          +"Play again?", result, JOptionPane.YES_NO_OPTION)
-          !=JOptionPane.YES_OPTION) {
-        System.exit(0);
-      }
+}
 
-      // Clear the board to start a new game
-      for (int j=0; j<9; ++j)
-        position[j]=BLANK;
+if(ae.getSource()==jb9){
+if(flag==0){
+jb9.setIcon(cross);
+flag=1;x9=1;
+jl.setText("Player :2");
+}
+else{
+jb9.setIcon(circle);
+flag=0;c9=1;
+jl.setText("Player :1");
+}
 
-      // Computer starts first every other game
-      if ((wins+losses+draws)%2 == 1)
-        nextMove();
-    }
-  } // end inner class Board
-} // end class TicTacToe
+}
+if(ae.getSource()==jb10){
+jf.setVisible(false);
+new Myclass();
+}
+
+if((c1==1&&c2==1&c3==1)||(x1==1&&x2==1&x3==1)){
+jb1.setIcon(new ImageIcon("green.png"));
+jb2.setIcon(new ImageIcon("green.png"));
+jb3.setIcon(new ImageIcon("green.png"));
+if(c1==1&&c2==1&c3==1){jl.setText("Player :2  WON");}
+else{jl.setText("Player :1  WON");}
+jb7.setEnabled(false);jb8.setEnabled(false);jb9.setEnabled(false);jb4.setEnabled(false);jb5.setEnabled(false);
+jb6.setEnabled(false);
+}
+if((c4==1&&c5==1&c6==1)||(x4==1&&x5==1&x6==1)){
+jb4.setIcon(new ImageIcon("green.png"));
+jb5.setIcon(new ImageIcon("green.png"));
+jb6.setIcon(new ImageIcon("green.png"));
+if(c4==1&&c5==1&c6==1){jl.setText("Player :2  WON");}
+else{jl.setText("Player :1  WON");}
+jb1.setEnabled(false);jb2.setEnabled(false);jb3.setEnabled(false);jb7.setEnabled(false);jb8.setEnabled(false);
+jb9.setEnabled(false);
+}
+if((c7==1&&c8==1&c9==1)||(x7==1&&x8==1&x9==1)){
+jb7.setIcon(new ImageIcon("green.png"));
+jb8.setIcon(new ImageIcon("green.png"));
+jb9.setIcon(new ImageIcon("green.png"));
+if(c7==1&&c8==1&c9==1){jl.setText("Player :2  WON");}
+else{jl.setText("Player :1  WON");}
+jb1.setEnabled(false);jb2.setEnabled(false);jb3.setEnabled(false);jb4.setEnabled(false);jb5.setEnabled(false);
+jb6.setEnabled(false);
+}
+if((c1==1&&c4==1&c7==1)||(x1==1&&x4==1&x7==1)){
+jb1.setIcon(new ImageIcon("green.png"));
+jb4.setIcon(new ImageIcon("green.png"));
+jb7.setIcon(new ImageIcon("green.png"));
+if(c1==1&&c4==1&c7==1){jl.setText("Player :2  WON");}
+else{jl.setText("Player :1  WON");}
+jb9.setEnabled(false);jb2.setEnabled(false);jb3.setEnabled(false);jb8.setEnabled(false);jb5.setEnabled(false);
+jb6.setEnabled(false);
+}
+if((c2==1&&c5==1&c8==1)||(x2==1&&x5==1&x8==1)){
+jb2.setIcon(new ImageIcon("green.png"));
+jb5.setIcon(new ImageIcon("green.png"));
+jb8.setIcon(new ImageIcon("green.png"));
+if(c2==1&&c5==1&c8==1){jl.setText("Player :2  WON");}
+else{jl.setText("Player :1  WON");}
+jb1.setEnabled(false);jb7.setEnabled(false);jb3.setEnabled(false);jb4.setEnabled(false);jb9.setEnabled(false);
+jb6.setEnabled(false);
+}
+if((c3==1&&c6==1&c9==1)||(x3==1&&x6==1&x9==1)){
+jb3.setIcon(new ImageIcon("green.png"));
+jb6.setIcon(new ImageIcon("green.png"));
+jb9.setIcon(new ImageIcon("green.png"));
+if(c3==1&&c6==1&c9==1){jl.setText("Player :2  WON");}
+else{jl.setText("Player :1  WON");}
+jb1.setEnabled(false);jb2.setEnabled(false);jb7.setEnabled(false);jb4.setEnabled(false);jb5.setEnabled(false);
+jb8.setEnabled(false);
+}
+if((c1==1&&c5==1&c9==1)||(x1==1&&x5==1&x9==1)){
+jb1.setIcon(new ImageIcon("green.png"));
+jb5.setIcon(new ImageIcon("green.png"));
+jb9.setIcon(new ImageIcon("green.png"));
+if(c1==1&&c5==1&c9==1){jl.setText("Player :2  WON");}
+else{jl.setText("Player :1  WON");}
+jb7.setEnabled(false);jb2.setEnabled(false);jb3.setEnabled(false);jb4.setEnabled(false);jb8.setEnabled(false);
+jb6.setEnabled(false);
+}
+if((c3==1&&c5==1&c7==1)||(x3==1&&x5==1&x7==1)){
+jb3.setIcon(new ImageIcon("green.png"));
+jb5.setIcon(new ImageIcon("green.png"));
+jb7.setIcon(new ImageIcon("green.png"));
+if(c3==1&&c5==1&c7==1){jl.setText("Player :2  WON");}
+else{jl.setText("Player :1  WON");}
+jb1.setEnabled(false);jb2.setEnabled(false);jb8.setEnabled(false);jb4.setEnabled(false);jb9.setEnabled(false);
+jb6.setEnabled(false);
+}
+if((c1==1|x1==1)&&(c3==1||x3==1)&&(c4==1||x4==1)&&(c5==1||x5==1)&&(c6==1||x6==1)&&(c7==1||x7==1)&&(c8==1||x8==1)&&(c9==1||x9==1)&&(c2==1||x2==1)){
+jl.setText("Draw :)");
+jb1.setEnabled(false);jb2.setEnabled(false);jb3.setEnabled(false);jb4.setEnabled(false);jb5.setEnabled(false);
+jb6.setEnabled(false);jb7.setEnabled(false);jb8.setEnabled(false);jb9.setEnabled(false);
+}
+
+}
+}
+
+MyListener ml=new MyListener();
+// define what should be done when an user performs certain operation
+jb1.addActionListener(ml);
+jb2.addActionListener(ml);
+jb3.addActionListener(ml);
+jb4.addActionListener(ml);
+jb5.addActionListener(ml);
+jb6.addActionListener(ml);
+jb7.addActionListener(ml);
+jb8.addActionListener(ml);
+jb9.addActionListener(ml);
+jb10.addActionListener(ml);
+}
+}
+class Game{
+public static void main(String arg[]){
+new Myclass();   //default constructor is invoked
+}
+}
